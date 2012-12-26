5618af767d6cd5eedafeb3e9c86823372488207c
Evandro L. Gonçalves <evandrolgoncalves@gmail.com>
2012-12-26 13:19:43 -0200

    inserting parameters and curl the 1.8.3 version of jquery

--- setup.sh
+++ setup.sh
@@ -1,22 +1,47 @@
 #!/bin/bash
+# ferramenta command line para startar projeto de novo componente javascript, com os arquivos comumente usados
+# author: Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
+# https://github.com/evandrolg
+# License: MIT
 
-echo -e 'qual o nome do projeto?'
-read name_project
+# varre todos os parametros passados na execução do programa
+while test -n "$1"
+do
+	case "$1" in
+		-n | --name ) # 
+			shift
+			name_project="$1"
+		;;
+
+		-j | --jquery )
+			tem_jquery=1
+		;;
+	esac
+	shift
+done
 
 PROJECTS=~/Projetos/pessoais/front-end
 FILES_SETUP=$PROJECTS/setup-project/files
 
 # cria diretorio com o nome do projeto
-cd $PROJECTS && mkdir $name_project
+cd $PROJECTS 
+mkdir $name_project
 NEW_PROJECT=$PROJECTS/$name_project
 
 # cria arquivo de source principal com o nome do projeto
 cd $NEW_PROJECT
-mkdir src && cd src/
+mkdir src 
+cd src/
 touch $name_project.js
-cd ..
+
+# baixa jquery para o projeto, caso tenha sido solicitado por parametro
+if test "$tem_jquery" = 1
+then
+	curl -L -O https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+fi
 
 # copia arquivos pre-configurados
+cd ..
 mkdir example
 cp $FILES_SETUP/.gitignore .gitignore
 cp $FILES_SETUP/Makefile Makefile
@@ -27,3 +52,5 @@ cd test/
 mv runner.html runner.$name_project.html
 cd spec/
 mv spec.js spec.$name_project.js
+
+echo -e '\033[32mOk!\033[m'
